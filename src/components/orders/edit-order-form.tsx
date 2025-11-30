322d8aa14ee88340a14de13333ac2b4b2fcf4458
mahmoud barakat <mahmoudba98a@gmail.com>
2025-11-30 14:26:02 +0000

    Try fixing this error: `Runtime Error: Error: useWatch is not defined. E

--- src/components/orders/edit-order-form.tsx
+++ src/components/orders/edit-order-form.tsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import { useForm } from 'react-hook-form';
+import { useForm, useWatch } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import {
