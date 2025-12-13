4f26ec5b6d7f06ac7a9bf2e80fbd339e3e428ab3
Amar Sweid <amarsweid397@gmail.com>
2025-12-13 16:54:41 +0000

    قم بوضعه ضمن قلب

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -6,15 +6,18 @@ export function BrandLogo() {
       viewBox="0 0 32 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className="text-primary"
     >
-      <circle cx="16" cy="16" r="14" stroke="currentColor" strokeWidth="2" />
       <path 
-        d="M11 21.5V11.5L16 16.5L21 11.5V21.5" 
-        stroke="currentColor" 
-        strokeWidth="2" 
-        strokeLinecap="round" 
-        strokeLinejoin="round" 
+        d="M16 29.875C16 29.875 3.125 21.25 3.125 12.125C3.125 7.00003 7.00003 3.12503 12.125 3.12503C15.125 3.12503 17.625 4.75003 19 6.87503C19.8333 5.35414 21.5833 3.12503 24.375 3.12503C28.5 3.12503 31.875 6.62503 31.875 11.25C31.875 19.5 16 29.875 16 29.875Z"
+        className="text-secondary"
+        fill="currentColor"
+      />
+      <path
+        d="M11 21V13L16 18L21 13V21"
+        stroke="hsl(var(--primary))"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
