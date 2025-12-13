9610bd147a82b30709bf43b808feb0a0a0f5a4e1
Amar Sweid <amarsweid397@gmail.com>
2025-12-13 16:41:14 +0000

    استبدل ايقونة التطبيق بهذه الصورة

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,22 +3,74 @@ export function BrandLogo() {
     <svg
       width="32"
       height="32"
-      viewBox="0 0 32 32"
+      viewBox="0 0 100 50"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className="text-primary"
     >
-      <rect width="32" height="32" rx="8" fill="currentColor" />
+      <rect width="100" height="50" fill="#71717A" />
       <path
-        d="M9 23V9H13.5C15.9853 9 18 11.0147 18 13.5V13.5C18 15.9853 15.9853 18 13.5 18H9"
-        stroke="hsl(var(--primary-foreground))"
-        strokeWidth="2"
+        d="M78.667 8.33301H68.7337V24.9997H78.667V8.33301Z"
+        fill="hsl(var(--primary))"
       />
       <path
-        d="M14 23L23 9"
-        stroke="hsl(var(--primary-foreground))"
-        strokeWidth="2"
-        strokeLinecap="round"
+        d="M65.4004 8.33301H55.467V24.9997H65.4004V8.33301Z"
+        fill="hsl(var(--primary))"
+      />
+      <path
+        d="M52.1338 8.33301H42.2004V16.6663H52.1338V8.33301Z"
+        fill="hsl(var(--primary))"
+      />
+      <path
+        d="M52.1338 16.666H42.2004V24.9993H52.1338V16.666Z"
+        fill="hsl(var(--primary))"
+      />
+      <path
+        d="M65.4004 16.666H55.467V24.9993H65.4004V16.666Z"
+        fill="hsl(var(--primary))"
+      />
+      <path
+        d="M68.7337 16.666H78.667V24.9993H68.7337V16.666Z"
+        fill="hsl(var(--primary))"
+      />
+      <path
+        d="M38.8662 8.33301V24.9997H8.33301C8.33301 24.9997 18.2663 24.9997 28.2002 24.9997C35.5332 24.9997 38.8662 20.833 38.8662 16.6663C38.8662 12.4997 35.5332 8.33301 28.2002 8.33301C20.8672 8.33301 8.33301 8.33301 8.33301 8.33301"
+        fill="hsl(var(--primary))"
+      />
+      <path
+        d="M60.4336 31.667H57.1002V39.167H60.4336V31.667Z"
+        fill="hsl(var(--primary-foreground))"
+      />
+      <path
+        d="M54.5171 31.667V39.167H55.4671V33.3337C55.4671 31.667 55.4671 31.667 54.5171 31.667Z"
+        fill="hsl(var(--primary-foreground))"
+      />
+      <path
+        d="M51.1836 31.667H47.8502V39.167H51.1836V31.667Z"
+        fill="hsl(var(--primary-foreground))"
+      />
+      <path
+        d="M45.2666 31.667V39.167H46.2166V33.3337C46.2166 31.667 46.2166 31.667 45.2666 31.667Z"
+        fill="hsl(var(--primary-foreground))"
+      />
+      <path
+        d="M41.9336 31.667H38.6002V39.167H41.9336V31.667Z"
+        fill="hsl(var(--primary-foreground))"
+      />
+      <path
+        d="M33.0166 31.667V39.167H36.35C36.35 39.167 36.35 34.167 36.35 33.3337C36.35 31.667 36.35 31.667 33.0166 31.667Z"
+        fill="hsl(var(--primary-foreground))"
+      />
+      <path
+        d="M29.6836 31.667H26.3502V39.167H29.6836V31.667Z"
+        fill="hsl(var(--primary-foreground))"
+      />
+      <path
+        d="M74.4502 31.667V32.5C74.4502 33.3333 73.5002 34.1667 71.8335 34.1667C70.1668 34.1667 69.2168 33.3333 69.2168 32.5V31.667H74.4502Z"
+        fill="hsl(var(--primary-foreground))"
+      />
+      <path
+        d="M71.8335 35.833H70.8835V39.1663H71.8335V35.833Z"
+        fill="hsl(var(--primary-foreground))"
       />
     </svg>
   );
