15b5d71b1563178011de853c062a6367d6e6e30b
QamarSweid <amarsweid397@gmail.com>
2026-02-27 22:06:47 +0300

    Update cookie-actions.ts

--- src/providers/cookie-actions.ts
+++ src/providers/cookie-actions.ts
@@ -1,15 +1,14 @@
+"use server";
 
-'use server';
-
-import { cookies } from 'next/headers';
-import { getUserById } from '@/lib/firebase-actions';
+import { cookies } from "next/headers";
+import { getUserById } from "@/lib/firebase-actions";
 
 export async function getUserFromCookie() {
-    const cookieStore = cookies();
-    const userId = cookieStore.get('user_id')?.value;
-    
-    if (userId) {
-        return await getUserById(userId);
-    }
-    return null;
+  const cookieStore = cookies();
+  const userId = (await cookieStore).get("user_id")?.value;
+
+  if (userId) {
+    return await getUserById(userId);
+  }
+  return null;
 }
